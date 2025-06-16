4b4edeceaa3915479d58113fa62236a239635cda
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-06-16 18:14:22 -0300

    ✏️ adding new routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import { route } from './routes'
 
 const app = express();
 
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+const previewFolder = path.join(__dirname, '..', 'uploads', 'preview');
+const hlsFolder = path.join(__dirname, '..', 'uploads', 'hls');
+
+app.use(cors());
+
+app.use('/uploads/hls', express.static(hlsFolder));
+app.use('/uploads/preview', express.static(previewFolder));
 app.use('/cdn', route);
 
 app.listen(3030, () => {
